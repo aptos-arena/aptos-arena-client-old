9e2fa9ae9a5d5ac1c9bd9bd0d378609e0f8f4a31
jason <jason@hedmans.org>
2023-02-19 16:31:14 +0100

    new game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,21 +2,20 @@ import React from 'react'
 
 import { useUnityContext, Unity } from 'react-unity-webgl';
 
-const Game = () => {
+const Game: React.FC = () => {
 
     const { unityProvider } = useUnityContext({
-        loaderUrl: "/build/Web.loader.js",
-        dataUrl: "/build/Web.data",
-        frameworkUrl: "/build/Web.framework.js",
-        codeUrl: "/build/Web.wasm"
+        loaderUrl: "/build/Shooter.loader.js",
+        dataUrl: "/build/Shooter.data",
+        frameworkUrl: "/build/Shooter.framework.js",
+        codeUrl: "/build/Shooter.wasm"
     });
     
     return (
         <Unity 
             unityProvider={unityProvider} 
             style={{
-                height: 600,
-                width: 800
+                width: '100%'
             }}
         />
     )
